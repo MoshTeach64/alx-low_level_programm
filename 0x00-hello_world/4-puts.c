6e21ed2f93306989e1e0c273a8d3ef0fb6ae847d
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2022-09-08 21:13:14 -0700

    modified: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * *Description: print "Programming is like building a multilingual puzzle"
+ *
+ * *
+ *
+ * * Return: returns value 0
+ *
+ * */
+
+int main(void)
+{
+	puts("\'Programming is like building a multilingual puzzle");
+	return (0);
+}
