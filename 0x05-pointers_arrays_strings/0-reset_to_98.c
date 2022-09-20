4aee23359297acb846b543f9bf090eb401ad2bf5
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2022-09-19 21:57:06 -0700

    modified: 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - reset_to_98
- * @n: pointer to print
- *
- * Return: on success 1.
+ *main - reset_to_98
+ * @n: - pointer to print
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
