45f8eee2541a31c7bbb4f17c9eef347f42fcf619
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2022-10-05 13:59:18 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -33,17 +33,15 @@ char *create_array(unsigned int size, char c)
 			counter++;
 		}
 		return (array);
+		}
 	}
-}
 /**
-* array = (char *)malloc(sizeof(char) * size);
-
-if (array != NULL)
-{
-while (size > 0)
+ * array = (char *)malloc(sizeof(char) * size);
+  if (array != NULL)
+ {
+  while (size > 0)
 	array[--size] = c;
-}
-
-return (array);
-*/
+ }
+ return (array);
+ */
 }
