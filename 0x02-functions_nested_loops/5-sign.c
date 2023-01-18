5fde6fa64d47999204c701d35d5b4099539cd5f1
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2023-01-18 02:41:06 -0800

    5-sign.c: modify

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,12 +17,12 @@ int print_sign(int n)
 	_putchar('+');
 	return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
@@ -30,4 +30,5 @@ int print_sign(int n)
 	}
 
 	_putchar('\n');
+return (0);
 }
