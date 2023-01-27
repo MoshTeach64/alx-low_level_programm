787e4a783299f74c5f53d9254126c540041305cc
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2023-01-27 05:07:32 -0800

    code corrected

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,19 +5,17 @@
  * Return: 1 whether it is, 0 otherwise
  */
 
-/*int _isupper(int c)
+int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 else
-{
 	return (0);
 }
-return (0);
-}*/
-int _isupper(int c)
+
+/*int _isupper(int c)
 {
 int upper;
 
@@ -29,4 +27,4 @@ else
         return (0);
 }
 return(0);
-}
+}*/
